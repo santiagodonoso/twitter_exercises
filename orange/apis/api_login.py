35f388c4750a0764c4a749a4bb5940176c1e683f
santiagodonoso <santiagodonoso@gmail.com>
2023-03-16 15:21:33 +0100

    Add files via upload

--- orange/apis/api_login.py
+++ orange/apis/api_login.py
@@ -1,35 +1,42 @@
 from bottle import post, request, response
 import x
+import bcrypt
+
 ##############################
 @post("/api-login")
 def _():
-    try:
-        # if user is logged, go to the profile page of that user
-        user = request.get_cookie("user", secret=x.COOKIE_SECRET)
-        if user: return {"info":"success login", "user_name":user["user_name"]}
-        # Validate
-        user_email = x.validate_user_email()
-        x.validate_user_password()
-        # Connect to database
-        db = x.db()
-        user = db.execute("SELECT * FROM users WHERE user_email = ? LIMIT 1", (user_email,)).fetchone()
-        if not user: raise Exception(400, "Cannot login")
-        try:
-            import production
-            is_cookie_https = True
-        except:
-            is_cookie_https = False
-        response.set_cookie("user", user, secret=x.COOKIE_SECRET, httponly=True, secure=is_cookie_https)
-        return {"info":"success login", "user_name":user["user_name"]}
-    except Exception as e:
-        print(e)
-        try: # Controlled exception, usually comming from the x file
-            response.status = e.args[0]
-            return {"info":e.args[1]}
-        except: # Something unknown went wrong
-            response.status = 500
-            return {"info":str(e)}
-    finally:
-        if "db" in locals(): db.close()
+	try:
+		# if user is logged, return message to the API
+		user = request.get_cookie("user", secret=x.COOKIE_SECRET)
+		if user: return {"info":"success login", "user_name":user["user_name"]}
+		# Validate
+		user_email = x.validate_user_email()
+		user_password = x.validate_user_password()
+		# Connect to database
+		db = x.db()
+		user = db.execute("SELECT * FROM users WHERE user_email = ? LIMIT 1", (user_email,)).fetchone()
+		print("#"*30)
+		print(user)
+		if not user: raise Exception(400, "Cannot login")
+		if not bcrypt.checkpw(user_password.encode("utf-8"), user["user_password"]):
+			raise Exception(400, "Invalid credentials")
+		try:
+			import production
+			is_cookie_https = True
+		except:
+			is_cookie_https = False
+		user.pop("user_password") # Do not put the password in the cookie
+		response.set_cookie("user", user, secret=x.COOKIE_SECRET, httponly=True, secure=is_cookie_https)
+		return {"info":"success login", "user_name":user["user_name"]}
+	except Exception as e:
+		print(e)
+		try: # Controlled exception, usually comming from the x file
+			response.status = e.args[0]
+			return {"info":e.args[1]}
+		except: # Something unknown went wrong
+			response.status = 500
+			return {"info":str(e)}
+	finally:
+		if "db" in locals(): db.close()
 
 
--- orange/apis/api_sign_up.py
+++ orange/apis/api_sign_up.py
@@ -2,28 +2,31 @@ from bottle import post, response
 import x
 import uuid
 import time
+import bcrypt
+
 ##############################
 @post("/api-sign-up")
 def _():
-    try:
-        user_email = x.validate_user_email()
-        user_name = x.validate_user_name()
-        user_password = x.validate_user_password()
-        x.validate_user_confirm_password()
-
-        user_id = str(uuid.uuid4()).replace("-","")
-        user = {
+	try:
+		user_email = x.validate_user_email()
+		user_name = x.validate_user_name()
+		user_password = x.validate_user_password()
+		x.validate_user_confirm_password()
+		# Adding the salt to password
+		salt = bcrypt.gensalt()
+		user_id = str(uuid.uuid4()).replace("-","")
+		user = {
             "user_id" : user_id,
             "user_email" : user_email,            
             "user_name" : user_name,
             "user_created_at" : int(time.time()),
             "user_verification_key" : str(uuid.uuid4()).replace("-",""),
-            "user_password": user_password,
+            "user_password": bcrypt.hashpw(user_password.encode('utf-8'), salt),
             "user_first_name" : "",
             "user_last_name" : "",
             "user_verified_at" : 0,
-            "user_banner" : "",
-            "user_avatar" : "",
+            "user_banner" : 0,
+            "user_avatar" : 0,
             "user_total_tweets" : 0,
             "user_total_retweets" : 0,
             "user_total_comments" : 0,
@@ -32,40 +35,35 @@ def _():
             "user_total_followers" : 0,
             "user_total_following" : 0
         }
-        # create placed holders for values
-        values = ""
-        for key in user:
-            values += f":{key},"
-        values = values.rstrip(",")
-        print(values)
-
-        db = x.db()
-        total_rows_inserted = db.execute(f"INSERT INTO users VALUES({values})", user).rowcount        
-        # total_rows_inserted = db.execute("""
-        #     INSERT INTO users VALUES(:user_id, :user_email, :user_name, 
-        #     :user_created_at, :user_verification_key, :user_password, :user_first_name, 
-        #     :user_last_name, :user_verified_at, :user_banner, :user_avatar, 
-        #     :user_total_tweets, :user_total_retweets, :user_total_comments, :user_total_likes, :user_total_dislikes,
-        #     :user_total_followers, :user_total_following,)""", user).rowcount
-        if total_rows_inserted != 1: raise Exception("Please, try again")
-        return {
-            "info" : "user created", 
-            "user_id" : user_id
-        }
-    except Exception as e:
-        print(e)
-        try: # Controlled exception, usually comming from the x file
-            response.status = e.args[0]
-            return {"info":e.args[1]}
-        except: # Something unknown went wrong
-            if "user_email" in str(e): 
-                response.status = 400 
-                return {"info":"user_email already exists"}
-            if "user_name" in str(e): 
-                response.status = 400 
-                return {"info":"user_name already exists"}
-            # unknown scenario
-            response.status = 500
-            return {"info":str(e)}
-    finally:
-        if "db" in locals(): db.close()
+		# create placed holders for values
+		values = ""
+		for key in user:
+			values += f":{key},"
+		values = values.rstrip(",")
+		print(values)	
+		db = x.db()
+		total_rows_inserted = db.execute(f"INSERT INTO users VALUES({values})", user).rowcount        
+		if total_rows_inserted != 1: raise Exception("Please, try again")
+		db.commit()
+		return {
+			"info" : "user created", 
+			"user_id" : user_id
+		}
+	except Exception as e:
+		print("#"*30)
+		print(e)
+		try: # Controlled exception, usually comming from the x file
+			response.status = e.args[0]
+			return {"info":e.args[1]}
+		except: # Something unknown went wrong
+			if "user_email" in str(e): 
+				response.status = 400 
+				return {"info":"user_email already exists"}
+			if "user_name" in str(e): 
+				response.status = 400 
+				return {"info":"user_name already exists"}
+			# unknown scenario
+			response.status = 500
+			return {"info":str(e)}
+	finally:
+		if "db" in locals(): db.close()
